9e806a9b4ac14bbe4bfbd010ed5a12e283ad55e6
Bhavesh Patil <bhavesh.patil@perennialsys.com>
2023-02-04 22:35:33 +0530

    Updated Fruits, Flowers & crops data in EducationFragment.

--- app/src/main/java/com/project/agroworld/fragments/EducationFragment.java
+++ app/src/main/java/com/project/agroworld/fragments/EducationFragment.java
@@ -1,33 +1,193 @@
 package com.project.agroworld.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.project.agroworld.R;
+import com.project.agroworld.articles.ArticleDetailsActivity;
+import com.project.agroworld.articles.adapter.CropsAdapter;
+import com.project.agroworld.articles.adapter.FlowersAdapter;
+import com.project.agroworld.articles.adapter.FruitsAdapter;
+import com.project.agroworld.articles.listener.CropsClickListener;
+import com.project.agroworld.articles.listener.FlowerClickListener;
+import com.project.agroworld.articles.listener.FruitsClickListener;
+import com.project.agroworld.articles.model.CropsResponse;
+import com.project.agroworld.articles.model.FlowersResponse;
+import com.project.agroworld.articles.model.FruitsResponse;
+import com.project.agroworld.databinding.FragmentEducationBinding;
+import com.project.agroworld.ui.viewmodel.AgroViewModel;
+import com.project.agroworld.utils.CustomMultiColorProgressBar;
+
+import java.util.ArrayList;
 
 
-public class EducationFragment extends Fragment {
+public class EducationFragment extends Fragment implements CropsClickListener, FruitsClickListener, FlowerClickListener {
+
+    private FragmentEducationBinding binding;
+    private CropsAdapter cropsAdapter;
+    private FlowersAdapter flowersAdapter;
+    private FruitsAdapter fruitsAdapter;
+    private AgroViewModel viewModel;
+    private CustomMultiColorProgressBar progressBar;
+    private ArrayList<CropsResponse> cropsResponseArrayList = new ArrayList<>();
+    private ArrayList<FruitsResponse> fruitsResponseArrayList = new ArrayList<>();
+    private ArrayList<FlowersResponse> flowersResponseArrayList = new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_education, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_education, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
-        actionBar.setTitle("Agro world");
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        progressBar = new CustomMultiColorProgressBar(getContext(), "Please Wait..\nWe're running your request.");
+        viewModel = ViewModelProviders.of(this).get(AgroViewModel.class);
+        viewModel.init();
+        actionBar.setTitle("Education");
+        getCropsListFromApi();
+        getFruitsListFromApi();
+        getFlowersListFromApi();
+
+        binding.rvHowToExpandEd.setVisibility(View.GONE);
+        binding.tvHowToExpandEd.setText("How to expand data not found");
+    }
+
+    private void getCropsListFromApi() {
+        progressBar.showProgressBar();
+        viewModel.getCropsResponseLivedata().observe(getViewLifecycleOwner(), resource -> {
+            switch (resource.status) {
+                case ERROR:
+                    binding.rvCropsEd.setVisibility(View.GONE);
+                    binding.tvCropsEd.setVisibility(View.VISIBLE);
+                    binding.tvCropsEd.setText(resource.message);
+                    break;
+                case LOADING:
+                    break;
+                case SUCCESS:
+                    if (resource.data != null) {
+                        cropsResponseArrayList.clear();
+                        cropsResponseArrayList.addAll(resource.data);
+                        binding.rvCropsEd.setVisibility(View.VISIBLE);
+                        setRecyclerView();
+                    } else {
+                        binding.tvCropsEd.setVisibility(View.VISIBLE);
+                        binding.tvCropsEd.setText("Looks like Admin haven't added any item yet.");
+                    }
+                    break;
+            }
+        });
+    }
+
+
+    private void getFlowersListFromApi() {
+        viewModel.getFlowersResponseLivedata().observe(getViewLifecycleOwner(), resource -> {
+            switch (resource.status) {
+                case ERROR:
+                    progressBar.hideProgressBar();
+                    binding.rvFlowersEd.setVisibility(View.GONE);
+                    binding.tvFlowersEd.setVisibility(View.VISIBLE);
+                    binding.tvFlowersEd.setText(resource.message);
+                    break;
+                case LOADING:
+                    break;
+                case SUCCESS:
+                    if (resource.data != null) {
+                        flowersResponseArrayList.clear();
+                        flowersResponseArrayList.addAll(resource.data);
+                        progressBar.hideProgressBar();
+                        binding.rvFlowersEd.setVisibility(View.VISIBLE);
+                        setFlowersRecyclerView();
+                    } else {
+                        binding.tvFlowersEd.setVisibility(View.VISIBLE);
+                        binding.tvFlowersEd.setText("Looks like Admin haven't added any item yet.");
+                    }
+                    break;
+            }
+        });
+    }
+
+    private void getFruitsListFromApi() {
+        viewModel.getFruitsResponseLivedata().observe(getViewLifecycleOwner(), resource -> {
+            switch (resource.status) {
+                case ERROR:
+                    binding.rvFruitsEd.setVisibility(View.GONE);
+                    binding.tvFruitsEd.setVisibility(View.VISIBLE);
+                    binding.tvFruitsEd.setText(resource.message);
+                    break;
+                case LOADING:
+                    break;
+                case SUCCESS:
+                    if (resource.data != null) {
+                        fruitsResponseArrayList.clear();
+                        fruitsResponseArrayList.addAll(resource.data);
+                        binding.rvFruitsEd.setVisibility(View.VISIBLE);
+                        setFruitsRecyclerView();
+                    } else {
+                        binding.tvFruitsEd.setVisibility(View.VISIBLE);
+                        binding.tvFruitsEd.setText("Looks like Admin haven't added any item yet.");
+                    }
+                    break;
+            }
+        });
+    }
+
+    private void setRecyclerView() {
+        cropsAdapter = new CropsAdapter(cropsResponseArrayList, this);
+        binding.rvCropsEd.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        binding.rvCropsEd.setAdapter(cropsAdapter);
+    }
+
+    private void setFruitsRecyclerView() {
+        fruitsAdapter = new FruitsAdapter(fruitsResponseArrayList, this);
+        binding.rvFruitsEd.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        binding.rvFruitsEd.setAdapter(fruitsAdapter);
+    }
+
+    private void setFlowersRecyclerView() {
+        flowersAdapter = new FlowersAdapter(flowersResponseArrayList, this);
+        binding.rvFlowersEd.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        binding.rvFlowersEd.setAdapter(flowersAdapter);
+    }
+
+    @Override
+    public void onCropsClick(CropsResponse response) {
+        Intent intent = new Intent(getContext(), ArticleDetailsActivity.class);
+        intent.putExtra("itemResponse", response);
+        intent.putExtra("isCropResponse", true);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onFlowersClick(FlowersResponse response) {
+        Intent intent = new Intent(getContext(), ArticleDetailsActivity.class);
+        intent.putExtra("flowerItemResponse", response);
+        intent.putExtra("isFlowersResponse", true);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onFruitClick(FruitsResponse response) {
+        Intent intent = new Intent(getContext(), ArticleDetailsActivity.class);
+        intent.putExtra("fruitItemResponse", response);
+        intent.putExtra("isFruitResponse", true);
+        startActivity(intent);
     }
 }
